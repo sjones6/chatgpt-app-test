bc6166397276c056a5937e688125d0c6ef2e64e5
Spencer Jones <jones.spencera@gmail.com>
2025-12-01 23:08:53 -0500

    use public env vars

--- baseUrl.ts
+++ baseUrl.ts
@@ -2,6 +2,6 @@ export const baseURL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
     : "https://" +
-      (process.env.VERCEL_ENV === "production"
-        ? process.env.VERCEL_PROJECT_PRODUCTION_URL
-        : process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL);
+      (process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+        ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+        : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL || process.env.NEXT_PUBLIC_VERCEL_URL);
